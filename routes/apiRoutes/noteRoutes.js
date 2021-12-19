a4a3e58b565994e3b4a907200a4884c1b08f2fd3
unknown <44147809+LSabin23@users.noreply.github.com>
2021-12-19 13:52:06 -0500

    api routing troubleshooting

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -6,12 +6,18 @@ const notesObj = require('../../db/db.json')
 
 // add GET /api/notes route to read db.json file and return all saved notes as JSON
 router.get('/notes', (req, res) => {
-  let results = notesObj
-  res.json(results)
+  console.log('noteRoutes GET request')
+  // res.json(notesObj)
 })
 
 // add POST /api/notes route to receive new note to save on request body, add it to the db.json file, and return new note to client
 // need to give new notes unique ids
+router.post('/notes', (req, res) => {
+  // get new note title and text
+  // assign new note a unique id
+  // write completed note to db.json
+  // return new note to client
+})
 
 // OPTIONAL: create DELETE /api/notes/:id route
 
